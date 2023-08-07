856d3889f7e81f3ca55eee48dd9715f2f66ad15f
Jakub Majewski <majewski.j01@gmail.com>
2023-08-07 13:55:00 +0200

    feat: install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  
-
-  return (
-    <>
-     
-    </>
-  )
+    return (
+        <>
+            <div className='bg-blue-600'>ABC</div>
+        </>
+    );
 }
 
-export default App
+export default App;
