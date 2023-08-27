fac0f9f3aab20380d71e1a739bcec67f6c041791
Jakub Majewski <majewski.j01@gmail.com>
2023-08-27 18:28:57 +0200

    quick fix

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function Button({ text }) {
+type Props = {
+    text: string;
+};
+
+function Button({ text }: Props) {
     return (
         <button className='bg-orange-500 text-white text-2xl p-2 my-2 mx-3'>
-           {text}
+            {text}
         </button>
     );
 }
