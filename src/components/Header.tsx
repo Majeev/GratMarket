2c1b8c622b28c6d80bf766ae716498927a965c1f
Jakub Majewski <majewski.j01@gmail.com>
2023-08-27 14:16:04 +0200

    feat: build header for mobiles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+            <div className='bg-white flex justify-between'>
+              <h1 className='text-orange-500 font-bold text-4xl'>GratMarket</h1>
+              <button className='bg-orange-500 text-white text-2xl p-2'>Zaloguj sie</button>
+            </div>
+        </>
+    );
 }
 
-export default Header
+export default Header;
