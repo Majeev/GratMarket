677c313221a8e8694466be8a26a08a7f61ececd5
Jakub Majewski <majewski.j01@gmail.com>
2023-09-16 12:15:39 +0200

    feat: implement login popup for mobiles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,31 @@
 import { RiAccountCircleLine } from 'react-icons/Ri';
 import { useState } from 'react';
-import Button from './UI/Button.jsx'
+import Button from './UI/Button.jsx';
 
 function Header() {
     const [login, setLogin] = useState(false);
 
-
     const handleLogin = () => {
-      setLogin(!login)
-    }
+        setLogin(!login);
+    };
     return (
         <>
-            <div className='bg-white flex justify-between'>
+            <div className='bg-white flex justify-between items-center py-3'>
                 <h1 className='text-orange-500 font-bold text-4xl mx-2'>
                     GratMarket
                 </h1>
-                <button className='text-4xl mx-2' onClick={handleLogin} >
+                <button
+                    className={`text-5xl mx-2 ${login && 'text-orange-500'} `}
+                    onClick={handleLogin}>
                     <RiAccountCircleLine />
                 </button>
             </div>
-           {login && <div className='flex flex-col bg-white w-3/6 m-5 p-2'>
-                <Button text={'Zaloguj sie'} />
-                <Button text={'Zarejestruj sie'} />
-            </div> }
+            {login && (
+                <div className='flex flex-col bg-white w-3/6 m-3 p-2 absolute right-0'>
+                    <Button text={'Zaloguj sie'} />
+                    <Button text={'Zarejestruj sie'} />
+                </div>
+            )}
         </>
     );
 }
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function Button({ text }: Props) {
     return (
-        <button className='bg-orange-500 text-white text-2xl p-2 my-2 mx-3'>
+        <button className='bg-orange-500 text-white text-2xl p-3 my-2 mx-3'>
             {text}
         </button>
     );
