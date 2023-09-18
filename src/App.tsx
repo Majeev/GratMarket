5472a127faf4d4c9f2fa4de1ce0c4f4cd6eb800c
Jakub Majewski <majewski.j01@gmail.com>
2023-09-18 14:00:06 +0200

    configure home page as a root page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
-import Header from './components/Header';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
-            <Header />
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     );
 }
 
