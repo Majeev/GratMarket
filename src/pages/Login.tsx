5422d88c13476eb20a081b391388ce93936d561a
Jakub Majewski <majewski.j01@gmail.com>
2023-09-18 15:07:01 +0200

    build login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Login() {
-    return <div>Login</div>;
+    return (
+        <div className='flex flex-col justify-center items-center '>
+        <h1>Założ Konto</h1>
+        <div className='w-2/6 bg-white h-20'></div>
+        </div>
+    );
 }
 
 export default Login;
